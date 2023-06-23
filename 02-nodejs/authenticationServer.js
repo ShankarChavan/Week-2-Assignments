8c6ab40cd0bb6c9d18aa01ff52bc178711be9e4f
ShankarChavan <ShankarChavan@users.noreply.github.com>
2023-06-23 11:13:43 +0000

    Completed assignments

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -1,37 +1,104 @@
-/**
-  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
-  - Don't need to use any database to store the data.
 
-  - Save the users and their signup/login data in an array in a variable
-  - You can store the passwords in plain text (as is) in the variable for now
+  // You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
+  // - Don't need to use any database to store the data.
 
-  The expected API endpoints are defined below,
-  1. POST /signup - User Signup
-    Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
-    Request Body: JSON object with username, password, firstName and lastName fields.
-    Response: 201 Created if successful, or 400 Bad Request if the username already exists.
-    Example: POST http://localhost:3000/signup
+  // - Save the users and their signup/login data in an array in a variable
+  // - You can store the passwords in plain text (as is) in the variable for now
 
-  2. POST /login - User Login
-    Description: Gets user back their details like firstname, lastname and id
-    Request Body: JSON object with username and password fields.
-    Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
-    Example: POST http://localhost:3000/login
+  // The expected API endpoints are defined below,
+  // 1. POST /signup - User Signup
+  //   Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
+  //   Request Body: JSON object with username, password, firstName and lastName fields.
+  //   Response: 201 Created if successful, or 400 Bad Request if the username already exists.
+  //   Example: POST http://localhost:3000/signup
 
-  3. GET /data - Fetch all user's names and ids from the server (Protected route)
-    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
-    The users username and password should be fetched from the headers and checked before the array is returned
-    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
-    Example: GET http://localhost:3000/data
+  // 2. POST /login - User Login
+  //   Description: Gets user back their details like firstname, lastname and id
+  //   Request Body: JSON object with username and password fields.
+  //   Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
+  //   Example: POST http://localhost:3000/login
 
-  - For any other route not defined in the server return 404
+  // 3. GET /data - Fetch all user's names and ids from the server (Protected route)
+  //   Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
+  //   The users username and password should be fetched from the headers and checked before the array is returned
+  //   Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
+  //   Example: GET http://localhost:3000/data
 
-  Testing the server - run `npm run test-authenticationServer` command in terminal
- */
+  // - For any other route not defined in the server return 404
+
+  // Testing the server - run `npm run test-authenticationServer` command in terminal
+
 
 const express = require("express")
 const PORT = 3000;
 const app = express();
 // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+const bodyParser = require("body-parser");
+// const jwt = require("jsonwebtoken");
+
+app.use(bodyParser.json());
+
+const users = [];
+
+app.post("/signup", (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
+    res.status(400).send("Username already exists");
+  } else {
+    const newUser = {
+      email,
+      password,
+      firstName,
+      lastName,
+      id: users.length + 1,
+    };
+    users.push(newUser);
+    res.status(201).send("Signup successful");
+  }
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+
+  if (user) {
+    res.json({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+  });
+  }
+   else {
+      res.status(401).send("Invalid credentials");
+    }
+  
+});
+
+app.get("/data", (req, res) => {
+  const { email, password } = req.headers;
+  let usersToReturn = [];
+  const user = users.find((user) => user.email === email && user.password === password);
+  if (user) {
+    users.forEach((user) => {
+      usersToReturn.push({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      })
+    })
+
+    res.json({
+      users: usersToReturn
+      });
+  } 
+  else {
+    res.status(401).send("Unauthorized");
+  }
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
 
 module.exports = app;
